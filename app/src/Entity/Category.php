b9012059d6c30de2943204a088e9acd9ee5b41f1
wolwik <kosoglos34@gmail.com>
2026-06-16 21:58:46 +0200

    Validate Category entity.

--- app/src/Entity/Category.php
+++ app/src/Entity/Category.php
@@ -11,29 +11,58 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\Collection;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
 #[ORM\Table(name: 'categories')]
-
 #[UniqueEntity(
     fields: ['name'],
     message: 'Taka kategoria już istnieje.'
 )]
 
-class Category {
+class Category
+{
+    /**
+     * Primary key.
+     */
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
+    /**
+     * Category name.
+     *
+     */
     #[ORM\Column(length: 255)]
+    #[Assert\Type('string')]
+    #[Assert\NotBlank(message: 'Nazwa kategorii nie może być pusta.')]
+    #[Assert\Length(
+        min: 1,
+        max: 255,
+        minMessage: 'Nazwa kategorii jest za krótka.',
+        maxMessage: 'Nazwa kategorii nie może być dłuższa niż {{ limit }} znaków.'
+    )]
     private ?string $name = null;
 
+    /**
+     * Category slug.
+     *
+     * Automatically generated.
+     */
     #[ORM\Column(length: 255)]
+    #[Assert\Type('string')]
     private ?string $slug = null;
 
+    /**
+     * Questions in this category.
+     *
+     * @var Collection<int, Question>
+     */
     #[ORM\OneToMany(targetEntity: Question::class, mappedBy: "category")]
+    #[Assert\Valid] // symfony walidując tą encję powinien też wejsc do tych obiektów i sprawdzić ich reguły walidacji
     private Collection $questions;
+
 
     public function __construct()
     {
