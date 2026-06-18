c78ffacc42e35224ee77a02772a4fd6fcd812a7e
wolwik <kosoglos34@gmail.com>
2026-06-19 00:25:52 +0200

    X

--- app/migrations/Version20260618221402.php
+++ app/migrations/Version20260618221402.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260617210049 extends AbstractMigration
+final class Version20260618221402 extends AbstractMigration
 {
     public function getDescription(): string
     {
--- app/src/Form/TagDeleteType.php
+++ app/src/Form/TagDeleteType.php
@@ -35,7 +35,7 @@ class TagDeleteType extends AbstractType
                 'data' => 'DELETE',
             ])
             ->add('delete', SubmitType::class, [
-                'label' => 'action.delete',
+                'label' => 'label.delete',
             ]);
     }
 }
